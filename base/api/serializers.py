fac844cb1ded3fb306dc61849052f2bb8d6ea7cf
Nayan-Sukhija <138364709+Nayan-Sukhija@users.noreply.github.com>
2023-07-30 18:57:26 +0530

    Add files via upload

---
+++ base/api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework.serializers import ModelSerializer
+from base.models import Room, Message
+
+
+class RoomSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+
+class MessageSerializer(ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
